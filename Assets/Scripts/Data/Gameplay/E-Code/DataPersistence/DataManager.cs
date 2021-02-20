5ada795cb3a6078035de8cdee48ac949c548b707
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2021-02-19 22:17:28 -0400

    Creating Data Manager/Persistence

--- Assets/Scripts/Data/Gameplay/E-Code/DataPersistence/DataManager.cs
+++ Assets/Scripts/Data/Gameplay/E-Code/DataPersistence/DataManager.cs
@@ -1,32 +1,110 @@
-﻿using System.Collections;
+﻿using System;
+using System.IO;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DataManager : MonoBehaviour
 {
+    public static DataManager data;
+
+    bool level0L;
+    bool level1L;
+    bool level2L;
+    bool level3L;
+    bool level4L;
+    bool level5L;
+    bool level6L;
+    bool level7L;
+    bool level8L;
+    bool level9L;
+    bool level10L;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        SingletonizeDataManager();
+    }
+    
+    private void SingletonizeDataManager()
+    {
+        if(data == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            data = this;
+            Debug.Log("Data Manager created");
+        }
+        else if(data != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UnlockLevel()
     {
-        
+
     }
+
+    public void SaveFile()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath +"/SaveData.dat");
+
+        Data levelData = new Data
+        {
+            level_0_Access = level0L,
+            level_1_Access = level1L,
+            level_2_Access = level2L,
+            level_3_Access = level3L,
+            level_4_Access = level4L,
+            level_5_Access = level5L,
+            level_6_Access = level6L,
+            level_7_Access = level7L,
+            level_8_Access = level8L,
+            level_9_Access = level9L,
+            level_10_Access = level10L
+        };
+
+        bf.Serialize(file, levelData);
+        file.Close();
+
+    }
+
+    public void LoadFile()
+    {
+        if (File.Exists(Application.persistentDataPath + "/SaveData.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/SaveData.dat", FileMode.Open);
+
+            Data unlockData = (Data)bf.Deserialize(file);
+        
+            file.Close();
+        }
+
+    }
+
+    public void EraseFile()
+    {
+
+    }
+
 }
 
+[Serializable]
 class Data
 {
-    bool Level_1_Unlocked;
-    bool Level_2_Unlocked;
-    bool Level_3_Unlocked;
-    bool Level_4_Unlocked;
-    bool Level_5_Unlocked;
-    bool Level_6_Unlocked;
-    bool Level_7_Unlocked;
-    bool Level_8_Unlocked;
-    bool Level_9_Unlocked;
-    bool Level_10_Unlocked;
+    public bool level_0_Access;
+    public bool level_1_Access;
+    public bool level_2_Access;
+    public bool level_3_Access;
+    public bool level_4_Access;
+    public bool level_5_Access;
+    public bool level_6_Access;
+    public bool level_7_Access;
+    public bool level_8_Access;
+    public bool level_9_Access;
+    public bool level_10_Access;
 }
