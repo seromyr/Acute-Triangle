93abb1028a64c20288f5f88997670557f8d3ed6a
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2021-03-10 20:39:01 -0400

    created Sound Manager

---
+++ Assets/Scripts/Data/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager sound;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SingletonizeSoundManager();
+        CreateAudioClips();
+    }
+
+    private void CreateAudioClips()
+    {
+        
+    }
+
+    private void SingletonizeSoundManager()
+    {
+        if(sound == null)
+        {
+            sound = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
