bf237884b1e1a802863580c44d266eb07a150c4b
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2021-04-01 14:47:37 -0300

    Pausing bug fixed

--- Assets/Scripts/UI Mechanic/MainMenu/UI_InGameMenu_Mechanic.cs
+++ Assets/Scripts/UI Mechanic/MainMenu/UI_InGameMenu_Mechanic.cs
@@ -231,6 +231,7 @@ public class UI_InGameMenu_Mechanic : MonoBehaviour
     }
 
     public void UsePause() => PauseGame();
+    public void UseResume() => ResumeGame();
 
     public void ResetPlayerHUD()
     {
